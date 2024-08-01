3bcc5e03fb60d1da3bd1358db16ad0856f44ffe4
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-08-01 13:57:15 +0700

    add demo ui for text input

--- app/test-ui/page.tsx
+++ app/test-ui/page.tsx
@@ -1,6 +1,12 @@
+"use client"
+import { useState } from "react";
 import Button from "../_components/button";
+import TextInput from "../_components/text-input";
 
 export default function TestUIPage() {
+
+    const [changableValue, setChangableValue] = useState("");
+
     return <div>
         <h1>Buttons</h1>
         <hr />
@@ -8,5 +14,15 @@ export default function TestUIPage() {
         <Button content="Button" size="md" /><br />
         <Button content="Button" size="lg" /><br />
         <Button content="Button" className="bg-blue-500 text-white" />
+        <Button content="Clickable Button" onClick={e => alert("you clicked!")} size="lg" />
+        <br />
+        <hr />
+        <h1>Text input</h1>
+        <br />
+
+        <TextInput type="text" placeholder="this is text field" />
+        <TextInput type="password" placeholder="this is pasword" />
+        <TextInput className="bg-orange-200 text-green-400" type="text" placeholder="this is customed text field" />
+        <TextInput value={changableValue} onChange={(e) => setChangableValue(e.target.value)} type="text" placeholder="Change by event" />
     </div>
 }
