f07bf12f359f73cf76ece19ed5275f816c025ea6
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-08-01 15:06:22 +0700

    add ui demo for label text input

--- app/test-ui/page.tsx
+++ app/test-ui/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Button from "../_components/button";
 import TextInput from "../_components/text-input";
+import LabelTextInput from "../_components/label-text-input";
 
 export default function TestUIPage() {
 
@@ -24,5 +25,39 @@ export default function TestUIPage() {
         <TextInput type="password" placeholder="this is pasword" />
         <TextInput className="bg-orange-200 text-green-400" type="text" placeholder="this is customed text field" />
         <TextInput value={changableValue} onChange={(e) => setChangableValue(e.target.value)} type="text" placeholder="Change by event" />
+        <br />
+        <h1>Text input</h1>
+        <br />
+        <LabelTextInput title="Label input" id="label-input-1" />
+        <LabelTextInput
+            title="Label input with place holder"
+            id="label-input-2"
+            placeholder="this is place holder"
+        />
+
+        <LabelTextInput
+            title="Customed label input"
+            id="label-input-3"
+            placeholder="this is place holder"
+            className="bg-orange-500 text-white placeholder-white"
+        />
+
+        <LabelTextInput
+            title="Use state label input"
+            id="label-input-4"
+            placeholder="this is place holder"
+            value={changableValue}
+            onChange={(e) => setChangableValue(e.target.value)}
+        />
+
+        <LabelTextInput
+            type="password"
+            title="Password label in put field"
+            id="label-input-4"
+            placeholder="this is place holder"
+            value={changableValue}
+            onChange={(e) => setChangableValue(e.target.value)}
+        />
+
     </div>
 }
