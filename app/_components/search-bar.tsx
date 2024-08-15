17f6da8396ed99d54319d47475019ef8e74fec7b
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-08-15 16:47:18 +0700

    #8 update search bar

--- app/_components/search-bar.tsx
+++ app/_components/search-bar.tsx
@@ -3,18 +3,32 @@
 import { useState } from "react";
 import TextInput from "./text-input";
 import Button from "./button";
-import { MagnifyingGlassCircleIcon, MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 
 export default function SerchBar() {
-
-    const [searchValue, setSearchValue] = useState('');
+    const searchParams = useSearchParams();
+    const pathName = usePathname();
+    const { replace } = useRouter();
+    const [serchTerm, setSearchTerm] = useState(searchParams.get('query')?.toString());
+    function handleSearch(term: string) {
+        const params = new URLSearchParams(searchParams);
+        if (term) {
+            params.set('query', term);
+        } else {
+            params.delete('query');
+        }
+        replace(`${pathName}?${params.toString()}`);
+        setSearchTerm(term);
+    }
     return <form className="flex border-2 border-solid border-blue-500 rounded-lg">
         <TextInput
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            onClearText={(e) => setSearchValue('')}
+            onChange={(e) => handleSearch(e.target.value)}
+            onClearText={(e) => handleSearch('')}
             placeholder="Search..."
             className="outline-none rounded-r-none bg-transparent"
+            value={serchTerm}
         />
         <Button className="rounded-l-none bg-blue-500"><MagnifyingGlassIcon height={20} width={20} /></Button>
     </form>
