8997d26279b798be821cac3a7a7cd2df9d170116
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-08-18 17:05:20 +0700

    #11 update components

--- app/_components/side-menu.tsx
+++ app/_components/side-menu.tsx
@@ -4,7 +4,7 @@ import BarsButton from "./bars-button";
 import SearchBar from "./search-bar";
 
 export default function SideMenu({ id, user }: Readonly<{ id: string, user: User }>) {
-    return <div id={id} className="hidden lg:block h-full bg-[#6b6b6b] fixed p-2 pt-4 w-[344px] z-10 shadow-lg">
+    return <div id={id} className="hidden lg:block h-full bg-[#6b6b6b] fixed p-2 pt-4 w-[344px] z-50 shadow-lg">
         <BarsButton toggleElementId="side-menu" className="relative top-[1.5px]" />
         <div className="flex flex-col gap-4 py-5 lg:py-0">
             <AccountInfo user={user} />
