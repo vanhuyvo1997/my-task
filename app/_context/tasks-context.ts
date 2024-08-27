fb1b52e30ffbed7320bc1d1ad17aa6617b253b43
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-08-27 19:49:27 +0700

    #16 add tasks related contexts

---
+++ app/_context/tasks-context.ts
@@ -0,0 +1,6 @@
+import { createContext, Dispatch } from "react";
+import { TaskData } from "../user/page";
+import { TasksAction } from "../_reducers/tasks-reducer";
+
+export const TasksContext = createContext<TaskData[]>([]);
+export const TasksDispatchContext = createContext<Dispatch<TasksAction>>(null!);
