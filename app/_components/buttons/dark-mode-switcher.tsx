a306effe9ca0fd982abb82b673e0e8625f60dfea
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-09-03 22:32:03 +0700

    #33 add dark mode switcher

---
+++ app/_components/buttons/dark-mode-switcher.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { MoonIcon, SunIcon } from "@heroicons/react/20/solid";
+import clsx from "clsx";
+import { useState } from "react"
+
+export default function DarkModeSwitcher() {
+    const [isOn, setIsOn] = useState(false);
+
+    function handleSwitch() {
+        const nextIsOn = !isOn;
+        if (nextIsOn) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+        setIsOn(nextIsOn);
+    }
+
+    return <button title="Toggle dark/light mode" onClick={handleSwitch} className="bg-gray-400 rounded-full w-[40px] h-[26px]">
+        <div className={
+            clsx("inline-block rounded-full", isOn ? 'float-right bg-green-500' : 'float-left bg-gray-500')
+        }>
+            {isOn ? <MoonIcon height={26} width={26} /> : <SunIcon height={26} width={26} />}
+        </div>
+    </button>
+}
