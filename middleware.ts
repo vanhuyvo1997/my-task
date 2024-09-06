ca3972a01f058444d0376820eca811aae1b4dcfb
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-09-06 19:24:54 +0700

    #45 restrict user from accessing unallowed pages
    (cherry picked from commit 2d268f7470c3292924195ba78a4ae991bb414182)

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,15 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "@/auth";
+
+export default auth(((req) => {
+    const pathname = req.nextUrl.pathname;
+    if (!req.auth && (pathname.startsWith('/tasks') || pathname.startsWith('/dashboard'))) {
+        const newUrl = new URL('/login', req.nextUrl.origin);
+        return Response.redirect(newUrl);
+    }
+
+    if (req.auth && (pathname === '/' || pathname.startsWith('/register') || pathname.startsWith('/login'))) {
+        const redirectTo = req.auth.user?.role === 'ADMIN' ? '/dashboard' : '/tasks';
+        const newUrl = new URL(redirectTo, req.nextUrl.origin);
+        return Response.redirect(newUrl);
+    }
+}));
