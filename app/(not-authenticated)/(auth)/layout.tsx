b60f3a2a4602dc8a08bc7dfa836f28270ca305e0
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-09-17 13:38:32 +0700

    change logo size

--- app/(not-authenticated)/(auth)/layout.tsx
+++ app/(not-authenticated)/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import Logo from '../../_components/layouts/logo';
 export default function AuthLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <div className="mt-10 mx-auto max-w-screen-sm flex flex-col items-center gap-3">
-            <Link href={'/'}><Logo width={'150'} /></Link>
+            <Link href={'/'}><Logo width={'100'} /></Link>
             <h1 className='text-2xl'>Let&apos;s create an account</h1>
             <div className="md:rounded-lg shadow-lg w-full p-10 bg-login-form-background-light dark:bg-login-form-background-dark">
                 {children}
