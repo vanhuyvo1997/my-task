be4b4f5f89d08d0cc53522ce00eeefb9554beb3e
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-09-20 23:38:09 +0700

    #69 update profile page

--- app/(authenticated)/profile/page.tsx
+++ app/(authenticated)/profile/page.tsx
@@ -1,21 +1,20 @@
 'use client'
+
 import Avatar from "@/app/_components/account/avatar";
 import OneLineLimitedText from "@/app/_components/layouts/limited-text";
-import { useSession } from "next-auth/react";
-
-
+import { useUserContext } from "@/app/_context/user-context";
 
 export default function ProfilePage() {
-    const session = useSession();
-    const user = session.data?.user;
+    const user = useUserContext();
+    const fullName = user?.firstName + " " + user?.lastName;
     return <div className="py-4 px-4 rounded-md bg-[#888888]">
         <div className="py-4">
             <div className="w-fit h-fit mx-auto">
-                <Avatar diameter="200" />
+                <Avatar diameter="200" avatarUrl={user?.avatarUrl} />
             </div>
         </div>
         <div className="flex flex-col gap-5 mx-auto w-full items-center">
-            <div className="text-3xl w-fit text-center">{user?.name}</div>
+            <div className="text-3xl w-fit text-center">{fullName}</div>
             <div className="max-w-full text-xl p-2">
                 <OneLineLimitedText title={user?.email ?? ''} className="w-full"><i><b>Email: </b>{user?.email}</i></OneLineLimitedText>
                 <i><b>Role:</b> {user?.role}</i>
