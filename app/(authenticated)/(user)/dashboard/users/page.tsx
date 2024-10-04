f22e20d459dbb81a07e7e6c9b5dfab672d1f8be7
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-10-04 10:05:06 +0700

    #79 add user management page

---
+++ app/(authenticated)/(user)/dashboard/users/page.tsx
@@ -0,0 +1,32 @@
+import UserRow from "@/app/_components/user/user-row";
+
+export default function ManageUsersPage() {
+    return <div>
+        <h1 className="text-2xl">User Mangement</h1>
+        <br />
+        <table className="border-collapse w-full text-left">
+            <thead className="py-4">
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Todo</th>
+                    <th>Done</th>
+                    <th>Total</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <UserRow />
+                <UserRow />
+                <UserRow />
+                <UserRow />
+                <UserRow />
+                <UserRow />
+                <UserRow />
+                <UserRow className="border-b-0" />
+            </tbody>
+        </table>
+
+
+    </div>
+}
--- app/(authenticated)/(user)/layout.tsx
+++ app/(authenticated)/(user)/layout.tsx
@@ -2,6 +2,8 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import HeaderBar from "../../_components/layouts/header-bar";
 import SideMenu from "../../_components/layouts/side-menu";
+import { ManageUsersMenuItem, StatisticsMenuItem } from "@/app/_components/layouts/menu-item";
+import React from "react";
 
 export default async function UserLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     const session = await auth();
@@ -10,7 +12,12 @@ export default async function UserLayout({ children }: Readonly<{ children: Reac
     }
     return <>
         <HeaderBar mode={session ? 'short' : 'full'} />
-        <SideMenu id='side-menu' />
+        <SideMenu id='side-menu'>
+            {session.user?.role === "ADMIN" && <>
+                <ManageUsersMenuItem />
+                <StatisticsMenuItem />
+            </>}
+        </SideMenu>
         <div className="w-auto min-w-[344px] lg:pl-[344px]">
             <div className="px-[2%] pt-20">
                 {children}
