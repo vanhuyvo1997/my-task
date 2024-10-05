1d261ba1a9f8679e76d33fea30fd56a7eed9d63f
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-10-05 17:14:01 +0700

    #79 add search bar to manage user page

--- app/(authenticated)/(user)/dashboard/users/page.tsx
+++ app/(authenticated)/(user)/dashboard/users/page.tsx
@@ -1,14 +1,16 @@
+import SearchBarV2 from "@/app/_components/text-inputs/search-bar-v2";
 import Pagination from "@/app/_components/user/pagination";
 import UserTable, { UserRowData } from "@/app/_components/user/user-table";
 import { auth } from "@/auth";
 
-export default async function ManageUserPage({ searchParams }: Readonly<{ searchParams: { page: number } }>) {
+export default async function ManageUserPage({ searchParams }: Readonly<{ searchParams: { page: number, query: string } }>) {
     const session = await auth();
     if (!session || session.error === 'RefreshAccessTokenError' || session.user?.role !== "ADMIN") {
         throw new Error("Permission denied");
     }
     const page = searchParams.page ?? 1;
-    const response = await fetch(`http://localhost:8080/api/users?pageSize=8&pageNum=${page - 1}&sortDir=desc`,
+    const query = searchParams.query;
+    const response = await fetch(`http://localhost:8080/api/users?pageSize=8&pageNum=${page - 1}&sortDir=desc${query ? "&query=" + query : ''}`,
         {
             headers: { "Authorization": "Bearer " + session.user.accessToken, },
         }
@@ -21,6 +23,10 @@ export default async function ManageUserPage({ searchParams }: Readonly<{ search
 
     return <div>
         <h1 className="text-2xl">User Mangement</h1>
+        <br />
+        <div className="md:relative">
+            <SearchBarV2 className="md:absolute md:right-4 md:-top-16" />
+        </div>
         <br />
         <UserTable userRowsData={rowsData} />
         <div className="py-5">
