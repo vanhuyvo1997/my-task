547dbb7a7f2d1b1dcbfaf17181828d67e45b2597
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-10-07 13:45:59 +0700

    #86 restructure statistics page

--- app/(authenticated)/(user)/dashboard/page.tsx
+++ app/(authenticated)/(user)/dashboard/page.tsx
@@ -1,9 +1,6 @@
-import { Metadata } from "next"
-
-export const metadata: Metadata = {
-    title: "Dashboard"
-}
-
-export default function AdminPage() {
-    return <>wellcome to admin</>
+export default function StatisticsPage() {
+    return <div>
+        <h1 className="text-2xl">Statistics</h1>
+        <br />
+    </div>
 }
