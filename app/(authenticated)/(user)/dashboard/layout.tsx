46590a0c95af268d08b8e17d1f96a7e5efaebb10
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-10-09 16:27:20 +0700

    #88 ad skeletons

--- app/(authenticated)/(user)/dashboard/layout.tsx
+++ app/(authenticated)/(user)/dashboard/layout.tsx
@@ -1,10 +1,7 @@
 import { ReactNode, Suspense } from "react";
-import Loading from "./loading";
 
 export default async function ManageUserLayout({ children }: Readonly<{ children: ReactNode }>) {
     return <div className="py-2">
-        <Suspense fallback={<Loading />}>
-            {children}
-        </Suspense>
+        {children}
     </div>
 }
--- app/(authenticated)/(user)/dashboard/page.tsx
+++ app/(authenticated)/(user)/dashboard/page.tsx
@@ -1,49 +1,21 @@
-import StatisticsColumn, { StatisticsDisplayData } from "@/app/_components/statistics/statistics-column";
+import StatisticsSectionContentSkeleton from "@/app/_components/skeletons/admin/statistics-section-content-skeleton";
+import UserRankedListSkeleton from "@/app/_components/skeletons/admin/user-ranked-list-skeleton";
 import UserRankedList from "@/app/_components/statistics/user-ranked-list";
 import AdminPageSection from "@/app/_components/user/admin-page-section";
-import { getStatistics } from "@/app/_dal/statistics-dal";
-import ChartThemeProviderWrapper from "@/app/_wrapper/chart-them-provider-wrapper";
-import { ListBulletIcon, UserGroupIcon } from "@heroicons/react/24/outline";
+import StatisticsSectionContent from "@/app/_components/user/statistics-section-content";
+import { Suspense } from "react";
 
 export default async function StatisticsPage() {
-    const { userStatistics, taskStatistics } = await getStatistics();
-
-    //Rename property to display
-    const { disabledUsers: Disabled, enabledUsers: Active } = userStatistics;
-    const { todoTasks: Todo, completedTasks: Complete } = taskStatistics;
-    const renamedUserStatistics = { Active, Disabled };
-    const renamedTaskStatistics = { Todo, Complete };
-
-    function generateStatisticDisplayData(obj: { [key: string]: number }) {
-        const result: StatisticsDisplayData[] = []
-        for (const [key, value] of Object.entries(obj)) {
-            result.push({ id: key, label: key, value: value });
-        }
-        return result;
-    }
-
-    const userStatisticsDisplayedData = generateStatisticDisplayData(renamedUserStatistics);
-    const taskStatisticsDisplayedData = generateStatisticDisplayData(renamedTaskStatistics);
-
     return <div className="flex flex-col gap-6">
-
         <AdminPageSection title="Top active users">
-            <UserRankedList />
+            <Suspense fallback={<UserRankedListSkeleton />}>
+                <UserRankedList />
+            </Suspense>
         </AdminPageSection>
-
         <AdminPageSection title="Statistics">
-            <div className="flex gap-2 flex-col lg:flex-row">
-                <ChartThemeProviderWrapper>
-                    <StatisticsColumn
-                        header={<> <span><UserGroupIcon className="h-6 w-6" /></span> Users</>}
-                        data={userStatisticsDisplayedData}
-                    />
-                    <StatisticsColumn
-                        header={<><span><ListBulletIcon className="h-6 w-6" /></span> Tasks</>}
-                        data={taskStatisticsDisplayedData}
-                    />
-                </ChartThemeProviderWrapper>
-            </div>
+            <Suspense fallback={<StatisticsSectionContentSkeleton />}>
+                <StatisticsSectionContent />
+            </Suspense>
         </AdminPageSection>
     </div>
 }
